0ab910386f74039731521b3173731d6d9451cc8f
tmashoro <102840195+tmashoro@users.noreply.github.com>
2022-06-20 21:04:54 -0500

    commit on feature branch

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const { animals } = require('./data/animals.')
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3001, () => {
+    console.log(`API server now on port 3001!`);
+});
+  
+app.get('/api/animals', (req, res) => {
+    res.send('Hello!');
+});
+  
